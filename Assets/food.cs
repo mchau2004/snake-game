8bbe78d6092d4d3336f385b79d1103e21dd03345
mchau2004 <84177780+mchau2004@users.noreply.github.com>
2021-10-26 12:14:00 +0700

    Add files via upload

---
+++ Assets/food.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    public BoxCollider2D gridarea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void RandomizePosition ()
+    {
+        Bounds bounds = this.gridarea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            RandomizePosition();
+        }
+    }
+}
